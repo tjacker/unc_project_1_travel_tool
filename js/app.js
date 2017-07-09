1de8f6b7bbc884fbcb57767158a79ac0d4964b49
Tim Acker <tjacker31@gmail.com>
2017-07-09 14:34:52 -0400

    Location Search
    Using geocomplete jQuery plugin added autocomplete search functionality to the input field. Also, used same plugin to add a google map. Finally, made some further styling changes to the header.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-$(document).foundation();
+/*jslint esversion: 6, browser: true*/
+/*global window, console, $, jQuery, alert*/
+
+const $searchFld = $('#search-fld');
+const $clearBtn = $('#clear-btn');
+const $locDetails = $('#location-details');
+const $map = $('#map');
+
+$searchFld.geocomplete({
+  map: $map,
+  details: $locDetails,
+  detailsAttribute: 'data-geo'
+});
+
+$clearBtn.click(function () {
+  $searchFld.val('');
+});
