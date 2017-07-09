6e04e70f165ffdead8058841e1049df7f80a989a
Zach Durham <ZachDurham@Zachs-MacBook-Pro.local>
2017-07-09 16:08:08 -0400

    added loading animation

--- js/app.js
+++ js/app.js
@@ -3,23 +3,20 @@ $(document).foundation();
 
 
 $(".button").on("click", function(e) {
-  
+  $(".tweet-content").html('<img src="img/loading.gif" width="100px" height="100px" align="center"/>')
   
   
   // Twitter Bearer Token = "Bearer  
   //AAAAAAAAAAAAAAAAAAAAANm41QAAAAAAymaZJR9slBVrP0CVnuDkNU1O2Wo%3D3mjc2Z1ym6zMyCWTY7uDiDIn7G// ivakZg7iGMlt70hZFQhzorUE"
-  var apiKey = "&api_key=o1ZMYWBCZW2KAMcZwMxNMdIlN"
   var query = "%23" + $("input").val().trim()
   var limit = "&count=6"
   // Black magic that will save this api
-  var corsAnywhere = "https://cors.now.sh/"
-  var queryURL = corsAnywhere +  "https://api.twitter.com/1.1/search/tweets.json?q=" + query + limit;
-  console.log(queryURL)
+  var corsAnywhere = "https://cors.now.sh/" 
 
   
 
-  // Function to set headers
-  
+ 
+  // Variables for AJAX call
   var settings = {
   "async": true,
   "crossDomain": true,
@@ -31,13 +28,16 @@ $(".button").on("click", function(e) {
     //"postman-token": "729f86df-93ae-901e-e6c0-2ce5be0bb3c4"
     }
   }
-
+  // Ajax Call
   $.ajax(settings).done(function (response) {
     console.log(response);
+    setTimeout(function() {
+      $(".tweet-content").html(response.statuses[0].text)  
+    }, 3000)
     
-    var tweetContent = $("<p>")
-    tweetContent.html(response.statuses[0].text)
-    tweetContent.appendTo($("body"))
+    
+    
+    
 
   });
 }) 
