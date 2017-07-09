aee634ad2e06864c038860ce1564010865d07da4
Tim Acker <tjacker31@gmail.com>
2017-07-09 17:46:23 -0400

    Open Weather Map
    Created AJAX request to return weather for the searched location based on the latitude and longitude values returned by Google Maps API. Need to see is passing the request through Heroku's https server will solve the OWM's http only issue for the free service.

--- js/app.js
+++ js/app.js
@@ -6,12 +6,38 @@ const $clearBtn = $('#clear-btn');
 const $locDetails = $('#location-details');
 const $map = $('#map');
 
+const $geoLat = $('[data-geo="lat"]');
+const $geoLng = $('[data-geo="lng"]');
+
 $searchFld.geocomplete({
   map: $map,
   details: $locDetails,
   detailsAttribute: 'data-geo'
+}).bind('geocode:result', function (event) {
+  refreshData();
 });
 
 $clearBtn.click(function () {
   $searchFld.val('');
 });
+
+let refreshData = function () {
+  let apiKey = '2a6299aeb6ba1831330eb81d8e215b1d';
+  let latitude = $geoLat.text();
+  console.log(latitude);
+  let longitude = $geoLng.text();
+  let url = 'https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?';
+  url += $.param({
+    lat: latitude,
+    lon: longitude,
+    units: 'imperial',
+    APPID: apiKey
+  });
+  console.log(url);
+  $.ajax({
+    url: url,
+    method: 'GET'
+  }).done(function (weather) {
+    console.log(weather);
+  });
+};
