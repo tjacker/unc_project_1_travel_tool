2ab9c9f9fd0d385fe0bf75c621e242e6145dc23f
Zach Durham <ZachDurham@Zachs-MacBook-Pro.local>
2017-07-10 23:47:38 -0400

    tweets successfully embeded. Now to figure out how to not display images

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 $(document).foundation();
 
 
-
+// Button click funciton
 $(".button").on("click", function(e) {
   $(".tweet-content").html('<img src="img/loading.gif" width="100px" height="100px" align="center"/>')
   
   
-  // Twitter Bearer Token = "Bearer  
-  //AAAAAAAAAAAAAAAAAAAAANm41QAAAAAAymaZJR9slBVrP0CVnuDkNU1O2Wo%3D3mjc2Z1ym6zMyCWTY7uDiDIn7G// ivakZg7iGMlt70hZFQhzorUE"
+
   var query = "%23" + $("input").val().trim()
-  var limit = "&count=6"
-  // Black magic that will save this api
+  var limit = "&count="
+  var limitNum = 6
+  // Black magic that will save this api. Hardcoded into the url belowm but saved as a variable in case needed elsewhere.
   var corsAnywhere = "https://cors.now.sh/" 
 
   
@@ -20,50 +20,37 @@ $(".button").on("click", function(e) {
   var settings = {
   "async": true,
   "crossDomain": true,
-  "url": "https://cors.now.sh/https://api.twitter.com/1.1/search/tweets.json?q=" + "%23" + query + limit,
+  "url": "https://cors.now.sh/https://api.twitter.com/1.1/search/tweets.json?q=" + "%23" + query + limit + limitNum,
   "method": "GET",
   "headers": {
     "authorization": "Bearer AAAAAAAAAAAAAAAAAAAAANm41QAAAAAAymaZJR9slBVrP0CVnuDkNU1O2Wo%3D3mjc2Z1ym6zMyCWTY7uDiDIn7GivakZg7iGMlt70hZFQhzorUE",
     "cache-control": "no-cache",
-    //"postman-token": "729f86df-93ae-901e-e6c0-2ce5be0bb3c4"
+    //"postman-token": "729f86df-93ae-901e-e6c0-2ce5be0bb3c4" // Commented out to test if needed, appears to not be.
     }
   }
   // Ajax Call
   $.ajax(settings).done(function (response) {
     console.log(response);
-    setTimeout(function() {
-      $(".tweet-content").html(response.statuses[0].text)  
-    }, 3000)
-    
-    
-    
+    // Create Div that will hold all tweets. It will be within the container
+    var twitDiv = $("<div id='twitter-display'>")
+    twitDiv.appendTo($("#twitter-container"))
+
     
 
+    // For loop that will create the tweets according to the number of tweets returned from the API
+    for (var i = 0; i < response.statuses.length; i++) {
+      // Create tweet blocks dynamically. Each tweet is given an ID of "tweet-widget-i" where i is the number.
+      twttr.widgets.createTweet(response.statuses[i].id_str, document.getElementById("twitter-display"));
+      $(".EmbeddedTweet-tweet").attr("data-cards", "hidden")
+
+      
+      // This may be used later. Just commented out for now
+      /* setTimeout(function() {
+        $(".tweet").html(response.statuses[i].text)  
+      }, 3000) */
+    }
   });
 }) 
- // Below here find old garbage that did not work but I don't want to delete yet ///////////////////////////////////////////////////
 
- /* // Ajax call
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-    xhrFields: {
-      withCredentials: true
-    },
-    json: true,
-    dataType: "jsonp",
-    crossDomain: true,
-    headers: {
-      "Host": "api.twitter.com",
-      "consumer_key": "o1ZMYWBCZW2KAMcZwMxNMdIlN",
-      "consumer_secret": "P4Djio9KSlk49TBk40xKkzYJSfSbI8yPutkoah0a5xe6BtYY7B",
-      Authorization: "Bearer AAAAAAAAAAAAAAAAAAAAANm41QAAAAAAymaZJR9slBVrP0CVnuDkNU1O2Wo%3D3mjc2Z1ym6zMyCWTY7uDiDIn7GivakZg7iGMlt70hZFQhzorUE",
-      //"Access-Control-Allow-Origin": "*",
-    }
-    
-  })
-  .done(function(response) {
-    console.log(response)
-  }) */
 
-////////////////////////////////////////////////////    
+
