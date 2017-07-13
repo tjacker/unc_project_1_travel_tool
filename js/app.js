d73c0427a3b0a9886a6af27fa4b236f3c7e71bef
MB DR <mderoberts@users.noreply.github.com>
2017-07-13 19:10:19 -0400

    add news api

--- js/app.js
+++ js/app.js
@@ -537,4 +537,41 @@ let randomNumber = function (number) {
   return Math.floor(Math.random() * number) + 1;
 };
 
+// News API
+
+function getArticles() {
+    var location = "chicago";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=b374b0d5d62040c9a7db208eba9a3654&q=" + location + "&begin_date=20170701";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        "async": true,
+        "crossDomain": true
+    }).done(function(result) {
+        console.log(result);
+        console.log("URL: " + queryURL);
+        for (var i = 0; i < 5; i++) {
+            var headline = result.response.docs[i].headline.main;
+            var snippet = result.response.docs[i].snippet;
+            var thumbnailURL = result.response.docs[i].multimedia[2].url;
+            var url = result.response.docs[i].web_url;
+            console.log("headline: " + headline);
+            console.log("snippet: " + snippet);
+            console.log("thumbnail: " + thumbnailURL);
+            console.log("url: " + url);
+
+            var newDiv = $("<div>");
+            newDiv.append("<img src='https://static01.nyt.com/" + thumbnailURL + "'></img><a href='" + url + "' target='_blank'><h3>" + headline + "</h3></a><p>" + snippet + "</p>");
+
+            $("#news").append(newDiv);
+        }
+    }).fail(function(err) {
+  throw err;
+});
+}
+
+// var location = "chicago";
+getArticles();
+
 
